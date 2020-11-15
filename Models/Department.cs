9503d554ed6f6bbe4f8b30433e79833e8bcbc87d
unknown <adambomfim@gmail.com>
2020-11-15 14:36:23 +0000

    Deleting department

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,14 @@
-﻿namespace SalesWebMvcTwo.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvcTwo.Models
 {
     public class Department
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Name { get; set; }
+
     }
 }
